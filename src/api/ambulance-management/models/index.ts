bfb5f8c1e2364389f2f62e0ec9350f8bae414d74
myrres0 <karolinaskrypova@gmail.com>
2026-04-11 14:50:33 +0200

    openapi generated code

---
+++ src/api/ambulance-management/models/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ModelError';
+export * from './Vehicle';
+export * from './VehicleCreateRequest';
+export * from './VehicleStatus';
+export * from './VehicleStatusUpdateRequest';
+export * from './VehicleUpdateRequest';
